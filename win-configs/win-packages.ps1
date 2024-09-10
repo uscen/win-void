a364ab28ed83f612610a580a6595e28a40b9b2d2
u-shen <farah.ushen@gmail.com>
2024-09-10 14:48:37 +0100

    remove bloated software

--- win-configs/win-packages.ps1
+++ win-configs/win-packages.ps1
@@ -12,10 +12,14 @@
 Write-Output("Uninstalling more crap we probably don't want, like apps for OneDrive, Spotify, and Disney+...")
 winget uninstall "Cortana" --silent --accept-source-agreements
 winget uninstall "Disney+" --silent --accept-source-agreements
+winget uninstall "Windows Notepad" --silent --accept-source-agreements
 winget uninstall "Mail and Calendar" --silent --accept-source-agreements
 winget uninstall "Microsoft News" --silent --accept-source-agreements
 winget uninstall "Microsoft OneDrive" --silent --accept-source-agreements
 winget uninstall "Microsoft Tips" --silent --accept-source-agreements
+winget uninstall "Microsoft To Do" --silent --accept-source-agreements
+winget uninstall "Microsoft Sticky Notes" --silent --accept-source-agreements
+winget uninstall "Windows Clock" --silent --accept-source-agreements
 winget uninstall "MSN Weather" --silent --accept-source-agreements
 winget uninstall "Movies & TV" --silent --accept-source-agreements
 winget uninstall "Office" --silent --accept-source-agreements
@@ -25,6 +29,15 @@ winget uninstall "Windows Maps" --silent --accept-source-agreements
 winget uninstall "Xbox TCUI" --silent --accept-source-agreements
 winget uninstall "Xbox Game Bar Plugin" --silent --accept-source-agreements
 winget uninstall "Xbox Game Bar" --silent --accept-source-agreements
+winget uninstall "Game Bar" --silent --accept-source-agreements
+winget uninstall "Xbox" --silent --accept-source-agreements
+winget uninstall "Solitaire & Casual Games" --silent --accept-source-agreements
+winget uninstall "Gaming Services" --silent --accept-source-agreements
+winget uninstall "Get Help" --silent --accept-source-agreements
+winget uninstall "Microsoft Clipchamp" --silent --accept-source-agreements
+winget uninstall "Feedback Hub" --silent --accept-source-agreements
+winget uninstall "Phone Link" --silent --accept-source-agreements
+winget uninstall "Microsoft People" --silent --accept-source-agreements
 winget uninstall "Xbox Identity Provider" --silent --accept-source-agreements
 winget uninstall "Xbox Game Speech Window" --silent --accept-source-agreements
 winget uninstall "Power Automate" --silent --accept-source-agreements
@@ -78,9 +91,15 @@ $packages = @(
 $scoopPackages = @(
     "aria2",
     "gcc",
-    "qutebrowser",
     "https://raw.githubusercontent.com/aandrew-me/tgpt/main/tgpt.json"
 )
+# Install Winget Packages:	                      #
+# ================================================#
+foreach ($package in $packages) {
+    Write-Host "Installing $package..."
+    winget install $package --exact --silent --accept-source-agreements --accept-package-agreements
+}
+Write-Host "Installation of Winget packages is complete!"
 # Install Scoop Package Manager:	              #
 # ================================================#
 if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
@@ -88,23 +107,8 @@ if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
     Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
     scoop bucket add extras
 }
-# Install Scoop Packages:	                      #
-# ================================================#
 foreach ($package in $scoopPackages) {
     Write-Host "Installing $package..."
-    try {
-        scoop install $package
-        Write-Host "$package installed successfully."
-    } catch {
-        Write-Host "Failed to install $package."
-    }
+    scoop install $package
 }
 Write-Host "Installation Of Scoop Packages Is Complete!"
-# Install Winget Packages:	                      #
-# ================================================#
-winget source update
-foreach ($package in $packages) {
-    Write-Host "Installing $package..."
-    winget install $package --exact --silent --accept-source-agreements --accept-package-agreements
-}
-Write-Host "Installation of Winget packages is complete!"
--- win-dotfiles/app/local/nvim/lua/plugins/dressing.lua
+++ win-dotfiles/app/local/nvim/lua/plugins/dressing.lua
@@ -4,24 +4,7 @@
 return {
 	"stevearc/dressing.nvim",
 	event = "VeryLazy",
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"nvim-telescope/telescope.nvim",
-	},
 	config = function()
-		require("dressing").setup({
-			select = {
-				telescope = require("telescope.themes").get_dropdown({
-					borderchars = {
-						prompt = { "─", "│", " ", "│", "┌", "┐", "│", "│" },
-						results = { "─", "│", "─", "│", "├", "┤", "┘", "└" },
-						preview = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-					},
-				}),
-			},
-			input = {
-				border = "single",
-			},
-		})
+		require("dressing").setup()
 	end,
 }
