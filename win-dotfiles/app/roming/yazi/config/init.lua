f89f8ba5c54f4f8760bf247c3778676aab87d71b
u-shen <farah.ushen@gmail.com>
2024-09-12 22:12:05 +0100

    reaclean yazi config

--- win-dotfiles/app/roming/yazi/config/init.lua
+++ win-dotfiles/app/roming/yazi/config/init.lua
@@ -1,52 +1,19 @@
----@diagnostic disable: undefined-global
+-- Show Size And mtime In StatusBar
+function Linemode:size_and_mtime()
+	local year = os.date("%Y")
+	local time = math.floor(self._file.cha.modified or 0)
 
--- Show user and hostname in top bar
-function Header:host()
-	if ya.target_family() ~= "unix" then
-		return ui.Line({})
-	end
-	return ui.Line({ ui.Span(ya.user_name() .. "@" .. ya.host_name()):fg("lightgreen"):bold(true), ui.Span(":") })
-end
-
-function Header:render(area)
-	self.area = area
-
-	local right = ui.Line({ self:count(), self:tabs() })
-	local left = ui.Line({
-		self:host(),
-		self:cwd(math.max(0, area.w - right:width())):fg("blue"),
-		ui.Span("/"):fg("blue"):bold(true),
-		ui.Span(tostring(cx.active.current.hovered.name)):fg("white"):bold(true),
-	})
-	return {
-		ui.Paragraph(area, { left }),
-		ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
-	}
-end
-
--- Show symlink path in status bar
-function Status:name()
-	local h = cx.active.current.hovered
-	if not h then
-		return ui.Span("")
+	if time > 0 and os.date("%Y", time) == year then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = time and os.date("%b %d  %Y", time) or ""
 	end
 
-	local linked = ""
-	if h.link_to ~= nil then
-		linked = " -> " .. tostring(h.link_to)
-	end
-	return ui.Span(" " .. h.name .. linked)
+	local size = self._file:size()
+	return ui.Line(string.format(" %s %s ", size and ya.readable_size(size) or "-", time))
 end
-
--- Remove file size from status bar
-function Status:render(area)
-	self.area = area
-
-	local left = ui.Line({ self:mode(), self:name() })
-	local right = ui.Line({ self:permissions(), self:position() })
-	return {
-		ui.Paragraph(area, { left }),
-		ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
-		table.unpack(Progress:render(area, right:width())),
-	}
-end
+-- Fully Border Plugins
+require("full-border"):setup({
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.ROUNDED,
+})
--- win-dotfiles/app/roming/yazi/config/plugins/smart-enter.yazi/init.lua
+++ win-dotfiles/app/roming/yazi/config/plugins/smart-enter.yazi/init.lua
@@ -1,19 +1,6 @@
----@diagnostic disable: undefined-global
-
 return {
-	entry = function(self, args)
+	entry = function()
 		local h = cx.active.current.hovered
-    if h and h.cha.is_dir then
-      ya.manager_emit("enter", {})
-      return
-    end
-
-    if #args > 0 and args[1] == "detatch" then
-      os.execute(string.format("opener detatch \"%s\"", h.url))
-    else
-      ya.manager_emit("open", {})
-      -- os.execute(string.format("opener \"%s\"", h.url))
-    end
+		ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
 	end,
 }
-
