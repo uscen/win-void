7b3e4e2520dab8cf4b24e6d318ee424cc744fc9a
u-shen <farah.ushen@gmail.com>
2024-09-22 18:57:32 +0100

    set gusses indent

--- win-dotfiles/app/local/nvim/lua/plugins/treesitter.lua
+++ win-dotfiles/app/local/nvim/lua/plugins/treesitter.lua
@@ -165,7 +165,19 @@ return {
 	{
 		"nmac427/guess-indent.nvim",
 		config = function()
-			require("guess-indent").setup({})
+			require("guess-indent").setup({
+				auto_cmd = true,
+				override_editorconfig = true,
+				on_tab_options = {
+					["expandtab"] = true,
+				},
+				on_space_options = {
+					["expandtab"] = true,
+					["tabstop"] = "detected",
+					["softtabstop"] = "detected",
+					["shiftwidth"] = "detected",
+				},
+			})
 		end,
 	},
 
