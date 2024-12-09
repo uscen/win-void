4eade276a3a1aa02485544de7b22e56725485f1d
u-shen <farah.ushen@gmail.com>
2024-12-09 20:07:11 +0100

    change term style

--- win-dotfiles/app/local/nvim/lua/plugins/toggleTerm.lua
+++ win-dotfiles/app/local/nvim/lua/plugins/toggleTerm.lua
@@ -13,7 +13,7 @@ return {
 			start_in_insert = true,
 			insert_mappings = true,
 			persist_size = true,
-			direction = "float",
+			direction = "horizontal",
 			close_on_exit = true,
 			shell = vim.o.shell,
 			shade_filetypes = { "none", "fzf" },
