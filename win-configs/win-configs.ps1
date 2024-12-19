7ffd36f51f0b678578aa1799dfda8cee565e1ef5
u-shen <farah.ushen@gmail.com>
2024-12-18 17:53:29 -0800

    remove config for keyboard rate not nedeed anymore

--- win-configs/win-configs.ps1
+++ win-configs/win-configs.ps1
@@ -34,11 +34,3 @@ Copy-Item -Path $Env:USERPROFILE\win-void\win-dotfiles\app\local\* -Destination 
 # ================================================#
 Copy-Item -Path $Env:USERPROFILE\win-void\win-dotfiles\others\docs\* -Destination $Env:USERPROFILE\Documents\ -Recurse -Force
 Copy-Item -Path $Env:USERPROFILE\win-void\win-dotfiles\others\wt\* -Destination $Env:localAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\ -Recurse -Force
-# ================================================#
-# Change Keybard reate:			                  #
-# ================================================#
-Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\Keyboard Response' -Name AutoRepeatDelay       -Value 200
-Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\Keyboard Response' -Name AutoRepeatRate        -Value 6
-Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\Keyboard Response' -Name DelayBeforeAcceptance -Value 0
-Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\Keyboard Response' -Name BounceTime            -Value 0
-Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\Keyboard Response' -Name Flags                 -Value 59
