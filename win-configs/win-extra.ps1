b9c0d9446a151705e3d6301d72e26adb8967209b
u-shen <farah.ushen@gmail.com>
2025-01-16 00:35:54 +0100

    clean unused files

--- win-configs/win-extra.ps1
+++ win-configs/win-extra.ps1
@@ -22,23 +22,12 @@ $extensions=@(
     "vscodevim.vim"
     "dsznajder.es7-react-js-snippets"
     "bradlc.vscode-tailwindcss",
-    "austenc.tailwind-docs",
     "formulahendry.auto-close-tag"
     "formulahendry.auto-rename-tag"
-    "steoates.autoimport"
     "glenn2223.live-sass"
-    "christian-kohler.path-intellisense"
     "esbenp.prettier-vscode"
-    "eamodio.gitlens"
-    "mhutchie.git-graph"
-    "mikestead.dotenv"
-    "tyriar.lorem-ipsum"
     "yzhang.markdown-all-in-one"
-    # Node/NPM/Yarn specific extensions
-    "christian-kohler.npm-intellisense"
-    "dbaeumer.vscode-eslint"
     # Themes
-    "github.github-vscode-theme"
     "sainnhe.gruvbox-material"
     "pkief.material-icon-theme"
 )
