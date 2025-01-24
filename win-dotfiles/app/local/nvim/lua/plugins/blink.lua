87b9317ee1713518a3828d7fb5cc6c4bf7a3072a
u-shen <farah.ushen@gmail.com>
2025-01-24 23:49:10 +0100

    added module

--- win-dotfiles/app/local/nvim/lua/plugins/blink.lua
+++ win-dotfiles/app/local/nvim/lua/plugins/blink.lua
@@ -5,6 +5,8 @@ return {
 	"saghen/blink.cmp",
 	dependencies = "rafamadriz/friendly-snippets",
 	version = "*",
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
 	opts = {
 		keymap = {
 			preset = "enter",
