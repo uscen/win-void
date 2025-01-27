4273a75d9b5439614004689206102697600b6085
u-shen <farah.ushen@gmail.com>
2025-01-27 15:18:18 +0100

    change to autoparis

--- win-dotfiles/app/local/nvim/lua/plugins/colorscheme.lua
+++ win-dotfiles/app/local/nvim/lua/plugins/colorscheme.lua
@@ -9,8 +9,9 @@ return {
 		vim.g.gruvbox_material_better_performance = 1
 		vim.cmd([[colorscheme gruvbox-material]])
 		-- changing bg and border colors
-		vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
+		vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
 		vim.api.nvim_set_hl(0, "BlinkCmpGhostText", { link = "Comment" })
+		vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
 		vim.api.nvim_set_hl(0, "LspInfoBorder", { link = "Normal" })
 		vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
 	end,
--- win-dotfiles/app/local/nvim/lua/plugins/leap.lua
+++ win-dotfiles/app/local/nvim/lua/plugins/leap.lua
@@ -6,7 +6,6 @@ return {
 		"ggandor/leap.nvim",
 		event = "VeryLazy",
 		config = function()
-			-- vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
 			vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
 			vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
 			vim.keymap.set({ "n", "x", "o" }, "gs", "<Plug>(leap-from-window)")
