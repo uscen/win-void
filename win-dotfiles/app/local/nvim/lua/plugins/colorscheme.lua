1c9c7cc1543acca911d89aa8c09d8c7d12c66c06
u-shen <farah.ushen@gmail.com>
2025-01-31 16:56:58 +0100

    change theme

--- win-dotfiles/app/local/nvim/lua/plugins/colorscheme.lua
+++ win-dotfiles/app/local/nvim/lua/plugins/colorscheme.lua
@@ -2,27 +2,21 @@
 -- Colorscheme
 -----------------------------------------------------------
 return {
-	"rebelot/kanagawa.nvim",
+	"sainnhe/gruvbox-material",
+	priority = 1000,
 	config = function()
-		require("kanagawa").setup({
-			compile = false,
-			terminalColors = true,
-			transparent = true,
-			dimInactive = false,
-			commentStyle = { italic = false },
-			keywordStyle = { italic = false },
-			statementStyle = { bold = false },
-			colors = {
-				theme = {
-					all = {
-						ui = {
-							bg_gutter = "none",
-						},
-					},
-				},
-			},
-		})
-		-- setup must be called before loading
-		require("kanagawa").load("dragon")
+		vim.g.gruvbox_material_background = "hard"
+		vim.g.gruvbox_material_foreground = "mix"
+		vim.g.gruvbox_material_enable_bold = false
+		vim.g.gruvbox_material_enable_italic = false
+		vim.g.gruvbox_material_disable_italic_comment = true
+		vim.g.gruvbox_material_better_performance = true
+		vim.cmd([[colorscheme gruvbox-material]])
+		-- changing bg and border colors
+		vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
+		vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
+		vim.api.nvim_set_hl(0, "LspInfoBorder", { link = "Normal" })
+		vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
+		vim.api.nvim_set_hl(0, "BlinkCmpGhostText", { link = "Comment" })
 	end,
 }
