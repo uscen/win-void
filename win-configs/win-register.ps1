efbde381fb4b2b57b4fa8ffbb684c9dde020d6b4
u-shen <farah.ushen@gmail.com>
2025-02-04 10:44:31 -0800

    remove keyboard rate script

--- win-configs/win-register.ps1
+++
@@ -1,8 +0,0 @@
-# ================================================#
-# Change Keybard reate:			                      #
-# ================================================#
-Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\Keyboard Response' -Name AutoRepeatDelay       -Value 200
-Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\Keyboard Response' -Name AutoRepeatRate        -Value 6
-Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\Keyboard Response' -Name DelayBeforeAcceptance -Value 0
-Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\Keyboard Response' -Name BounceTime            -Value 0
-Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\Keyboard Response' -Name Flags                 -Value 47
