ecd72e2ca1d65da3e6cedc7a9eea2a8f2a15b1bc
u-shen <farah.ushen@gmail.com>
2025-02-04 18:06:15 +0100

    to mini-pairs

---
+++ win-dotfiles/app/local/nvim/lua/plugins/mini-pairs.lua
@@ -0,0 +1,10 @@
+-----------------------------------------------------------
+-- Mini-Paris
+-----------------------------------------------------------
+return {
+  'echasnovski/mini.pairs',
+  version = '*',
+  config = function()
+    require('mini.pairs').setup()
+  end
+}
