6528f07142bd4a6a871b2387ae14d444940467ad
u-shen <farah.ushen@gmail.com>
2025-02-08 04:26:27 -0800

    fix tabline

--- win-dotfiles/app/local/nvim/lua/plugins/mini-tabline.lua
+++ win-dotfiles/app/local/nvim/lua/plugins/mini-tabline.lua
@@ -7,14 +7,6 @@ return {
   config = function()
     require('mini.tabline').setup({
       show_icons = true,
-      bold = false,
-      italic = {
-        strings = false,
-        emphasis = false,
-        comments = false,
-        operators = false,
-        folds = false,
-      },
     })
     vim.api.nvim_set_hl(0, "MiniTablineFill", { link = "Normal" })
     vim.api.nvim_set_hl(0, "MiniTablineCurrent", { link = "NormalFloat" })
