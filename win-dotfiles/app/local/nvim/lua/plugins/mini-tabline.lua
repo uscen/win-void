d7164516b1a2a044081df12fca2d8fd413f4aafe
u-shen <farah.ushen@gmail.com>
2025-02-08 04:39:16 -0800

    clean

--- win-dotfiles/app/local/nvim/lua/plugins/mini-tabline.lua
+++ win-dotfiles/app/local/nvim/lua/plugins/mini-tabline.lua
@@ -6,9 +6,7 @@ return {
   version = '*',
   event = "VeryLazy",
   config = function()
-    require('mini.tabline').setup({
-      show_icons = true,
-    })
+    require('mini.tabline').setup()
     vim.api.nvim_set_hl(0, "MiniTablineFill", { link = "Normal" })
     vim.api.nvim_set_hl(0, "MiniTablineCurrent", { link = "NormalFloat" })
     vim.api.nvim_set_hl(0, "MiniTablineVisible", { link = "Comment" })
