03b13df7e811510145cce57e7de003f40a4dd2b8
u-shen <farah.ushen@gmail.com>
2025-02-10 14:48:06 -0800

    added mini-statusline

--- win-dotfiles/app/local/nvim/lua/plugins/mini.lua
+++ win-dotfiles/app/local/nvim/lua/plugins/mini.lua
@@ -121,6 +121,21 @@ return {
     end
   },
   -----------------------------------------------------------
+  -- Mini-Statusline
+  -----------------------------------------------------------
+  {
+    "echasnovski/mini.statusline",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      local statusline = require 'mini.statusline'
+      statusline.setup { use_icons = true }
+      statusline.section_location = function()
+        return '%2l:%-2v'
+      end
+    end
+  },
+  -----------------------------------------------------------
   -- Mini-Starter
   -----------------------------------------------------------
   {
