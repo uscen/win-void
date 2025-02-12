b1ffb53b3fd79f87c2666ef7553eab9f2e2c84db
u-shen <farah.ushen@gmail.com>
2025-02-12 02:41:46 -0800

    added toggle yazi and ntop to nvim terminal

--- win-dotfiles/app/local/nvim/lua/plugins/FTerm.lua
+++ win-dotfiles/app/local/nvim/lua/plugins/FTerm.lua
@@ -22,11 +22,35 @@ return {
         width = 0.9
       }
     })
+    local yazi = fterm:new({
+      ft = 'fterm_yazi',
+      cmd = "yazi",
+      border = "double",
+      dimensions = {
+        height = 0.9,
+        width = 0.9
+      }
+    })
+    local htop = fterm:new({
+      ft = 'fterm_htop',
+      cmd = "ntop",
+      border = "double",
+      dimensions = {
+        height = 0.9,
+        width = 0.9
+      }
+    })
     vim.keymap.set('n', '<leader>tg', function()
       lazygit:toggle()
     end)
     vim.keymap.set('n', '<leader>lg', function()
       lazygit:toggle()
+    end)
+    vim.keymap.set('n', '<leader>ty', function()
+      yazi:toggle()
+    end)
+    vim.keymap.set('n', '<leader>th', function()
+      htop:toggle()
     end)
     vim.keymap.set('n', '<leader>td', '<CMD>lua require("FTerm").run("npm run dev")<CR>')
     vim.keymap.set('n', '<leader>tb', '<CMD>lua require("FTerm").run("npm run build")<CR>')
