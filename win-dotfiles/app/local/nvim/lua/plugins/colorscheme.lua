425f164efaee3ed1ba9e9afeaff456eda0d0a695
u-shen <farah.ushen@gmail.com>
2025-02-13 12:59:09 -0800

    added mini-tabline

--- win-dotfiles/app/local/nvim/lua/plugins/colorscheme.lua
+++ win-dotfiles/app/local/nvim/lua/plugins/colorscheme.lua
@@ -18,5 +18,9 @@ return {
     -- Change Highlighted Groups:
     vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
     vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
+    vim.api.nvim_set_hl(0, "MiniTablineFill", { link = "Normal" })
+    vim.api.nvim_set_hl(0, "MiniTablineCurrent", { link = "NormalFloat" })
+    vim.api.nvim_set_hl(0, "MiniTablineVisible", { link = "Comment" })
+    vim.api.nvim_set_hl(0, "MiniTablineHidden", { link = "Comment" })
   end
 }
