da50c8ca3f14f7e83da430234c0d5bc7dd222426
u-shen <farah.ushen@gmail.com>
2025-02-18 23:00:17 +0100

    remove nonssanse comment

--- win-dotfiles/app/local/nvim/lua/config/autocmds.lua
+++ win-dotfiles/app/local/nvim/lua/config/autocmds.lua
@@ -24,12 +24,9 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.nvim_create_user_command("OilToggle", function()
   local current_buf = vim.api.nvim_get_current_buf()
   local current_filetype = vim.api.nvim_buf_get_option(current_buf, "filetype")
-
   if current_filetype == "oil" then
-    -- We use a command to go to the previous buffer
     vim.cmd("b#")
   else
-    -- Open oil if not already in an oil buffer
     vim.cmd("Oil")
   end
 end, { nargs = 0 })
