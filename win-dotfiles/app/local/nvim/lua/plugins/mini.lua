ea873ac58ec623c3cb6b194e597ce0ef588c125d
u-shen <farah.ushen@gmail.com>
2025-02-18 12:10:57 +0100

    remove tabline

--- win-dotfiles/app/local/nvim/lua/plugins/mini.lua
+++ win-dotfiles/app/local/nvim/lua/plugins/mini.lua
@@ -62,22 +62,6 @@ return {
     opts = {}
   },
   -----------------------------------------------------------
-  -- Mini-tabline
-  -----------------------------------------------------------
-  {
-    'echasnovski/mini.tabline',
-    event = "VeryLazy",
-    version = '*',
-    config = function()
-      require('mini.tabline').setup()
-      vim.api.nvim_set_hl(0, "MiniTablineFill", { link = "Normal" })
-      vim.api.nvim_set_hl(0, "MiniTablineCurrent", { link = "Normal" })
-      vim.api.nvim_set_hl(0, "MiniTablineVisible", { link = "Comment" })
-      vim.api.nvim_set_hl(0, "MiniTablineHidden", { link = "Comment" })
-      vim.api.nvim_set_hl(0, "MiniTablineModifiedCurrent", { link = "CursorLine" })
-    end
-  },
-  -----------------------------------------------------------
   -- Mini-Pick
   -----------------------------------------------------------
   {
