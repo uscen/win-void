51a1ce95cbce6ee9643fc0e799857846cf1a2a1b
u-shen <farah.ushen@gmail.com>
2025-02-21 10:19:32 +0100

    disable nvim intro

--- win-dotfiles/app/local/nvim/lua/config/options.lua
+++ win-dotfiles/app/local/nvim/lua/config/options.lua
@@ -13,64 +13,65 @@ vim.schedule(function()
   vim.opt.clipboard = "unnamed,unnamedplus"
 end)
 
-vim.opt.compatible       = false
-vim.opt.swapfile         = false
-vim.opt.writebackup      = false
-vim.opt.backup           = false
-vim.opt.completeopt      = { "menu", "menuone", "preview" }
-vim.opt.undofile         = true
-vim.opt.undolevels       = 100
-vim.opt.timeoutlen       = 300
+vim.opt.compatible    = false
+vim.opt.swapfile      = false
+vim.opt.writebackup   = false
+vim.opt.backup        = false
+vim.opt.completeopt   = { "menu", "menuone", "preview" }
+vim.opt.undofile      = true
+vim.opt.undolevels    = 100
+vim.opt.timeoutlen    = 300
 -----------------------------------------------------------
 -- Shell
 -----------------------------------------------------------
-vim.opt.sh               = "nu"
-vim.opt.shellslash       = true
-vim.opt.shelltemp        = false
-vim.opt.shellcmdflag     = "--stdin --no-newline -c"
-vim.opt.shellredir       = "out+err> %s"
-vim.opt.shellxescape     = ""
-vim.opt.shellxquote      = ""
-vim.opt.shellquote       = ""
+vim.opt.sh            = "nu"
+vim.opt.shellslash    = true
+vim.opt.shelltemp     = false
+vim.opt.shellcmdflag  = "--stdin --no-newline -c"
+vim.opt.shellredir    = "out+err> %s"
+vim.opt.shellxescape  = ""
+vim.opt.shellxquote   = ""
+vim.opt.shellquote    = ""
 -----------------------------------------------------------
 -- Neovim UI
 -----------------------------------------------------------
-vim.opt.number           = true
-vim.opt.numberwidth      = 5
-vim.opt.splitright       = true
-vim.opt.splitbelow       = true
-vim.opt.termguicolors    = true
-vim.opt.ignorecase       = true
-vim.opt.smartcase        = true
-vim.opt.hlsearch         = true
-vim.opt.confirm          = true
-vim.opt.laststatus       = 0
-vim.opt.cmdheight        = 0
-vim.opt.wrap             = false
-vim.opt.showmode         = false
-vim.opt.fillchars        = { eob = " " }
-vim.opt.ruler            = false
-vim.wo.signcolumn        = "no"
+vim.opt.number        = true
+vim.opt.numberwidth   = 5
+vim.opt.splitright    = true
+vim.opt.splitbelow    = true
+vim.opt.termguicolors = true
+vim.opt.ignorecase    = true
+vim.opt.smartcase     = true
+vim.opt.hlsearch      = true
+vim.opt.confirm       = true
+vim.opt.laststatus    = 0
+vim.opt.cmdheight     = 0
+vim.opt.wrap          = false
+vim.opt.showmode      = false
+vim.opt.fillchars     = { eob = " " }
+vim.opt.ruler         = false
+vim.wo.signcolumn     = "no"
 --------------------------------------------------------
 -- Tabs, indent:
 -----------------------------------------------------------
-vim.opt.tabstop          = 2
-vim.opt.softtabstop      = 2
-vim.opt.shiftwidth       = 2
-vim.opt.expandtab        = true
-vim.opt.smartindent      = true
-vim.opt.shiftround       = true
+vim.opt.tabstop       = 2
+vim.opt.softtabstop   = 2
+vim.opt.shiftwidth    = 2
+vim.opt.expandtab     = true
+vim.opt.smartindent   = true
+vim.opt.shiftround    = true
 -----------------------------------------------------------
 -- Memory, CPU
 -----------------------------------------------------------
-vim.opt.hidden           = true
-vim.opt.history          = 100
-vim.opt.lazyredraw       = true
-vim.opt.synmaxcol        = 240
-vim.opt.updatetime       = 250
+vim.opt.hidden        = true
+vim.opt.history       = 100
+vim.opt.lazyredraw    = true
+vim.opt.synmaxcol     = 240
+vim.opt.updatetime    = 250
 -----------------------------------------------------------
 -- Startup
 -----------------------------------------------------------
+vim.opt.shortmess:append("sI")
 -- Disable builtin plugins
 local disabled_built_ins = {
   "osc52",
