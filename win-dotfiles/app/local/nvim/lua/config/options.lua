6c6a928b348dac762e4b314a09afaa58603aba48
u-shen <farah.ushen@gmail.com>
2025-02-25 13:31:55 -0800

    added usefual options

--- win-dotfiles/app/local/nvim/lua/config/options.lua
+++ win-dotfiles/app/local/nvim/lua/config/options.lua
@@ -6,6 +6,7 @@ vim.diagnostic.config({ signs = false, virtual_text = false })
 -- Set leader key:
 -----------------------------------------------------------
 vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 -----------------------------------------------------------
 -- General
 -----------------------------------------------------------
@@ -20,26 +21,34 @@ vim.cmd([[
 vim.schedule(function()
   vim.opt.clipboard = "unnamed,unnamedplus"
 end)
-vim.opt.completeopt   = { "menu", "menuone", "preview" }
-vim.opt.compatible    = false
-vim.opt.swapfile      = false
-vim.opt.writebackup   = false
-vim.opt.backup        = false
-vim.opt.undofile      = true
-vim.opt.undolevels    = 100
-vim.opt.timeoutlen    = 300
+vim.opt.completeopt  = { "menu", "menuone", "preview" }
+vim.opt.compatible   = false
+vim.opt.swapfile     = false
+vim.opt.writebackup  = false
+vim.opt.backup       = false
+vim.opt.undofile     = true
+vim.opt.undolevels   = 100
+vim.opt.timeoutlen   = 300
 -----------------------------------------------------------
 -- Shell
 -----------------------------------------------------------
-vim.opt.sh            = "nu"
-vim.opt.shellslash    = true
-vim.opt.shelltemp     = false
-vim.opt.shellcmdflag  = "--stdin --no-newline -c"
-vim.opt.shellredir    = "out+err> %s"
-vim.opt.shellxescape  = ""
-vim.opt.shellxquote   = ""
-vim.opt.shellquote    = ""
-vim.o.fillchars       = 'eob: '
+vim.opt.sh           = "nu"
+vim.opt.shellslash   = true
+vim.opt.shelltemp    = false
+vim.opt.shellcmdflag = "--stdin --no-newline -c"
+vim.opt.shellredir   = "out+err> %s"
+vim.opt.shellxescape = ""
+vim.opt.shellxquote  = ""
+vim.opt.shellquote   = ""
+vim.o.fillchars      = 'eob: '
+-----------------------------------------------------------
+-- Files and Others
+-----------------------------------------------------------
+vim.opt.termbidi     = false
+vim.opt.fileencoding = "utf-8"
+vim.opt.autochdir    = true
+vim.opt.whichwrap    = "b,s,<,>,[,],h,l"
+vim.opt.iskeyword:append("-,_")
 -----------------------------------------------------------
 -- Neovim UI
 -----------------------------------------------------------
@@ -54,13 +63,16 @@ vim.opt.ignorecase    = true
 vim.opt.smartcase     = true
 vim.opt.hlsearch      = true
 vim.opt.confirm       = true
+vim.opt.showmatch     = true
 vim.opt.laststatus    = 0
 vim.opt.cmdheight     = 0
-vim.opt.pumheight     = 12
+vim.opt.pumblend      = 0
+vim.opt.pumheight     = 8
 vim.opt.wrap          = false
 vim.opt.showmode      = false
 vim.opt.ruler         = false
 vim.wo.signcolumn     = "no"
+vim.opt.virtualedit   = "block"
 --------------------------------------------------------
 -- Tabs, indent:
 -----------------------------------------------------------
@@ -78,6 +90,11 @@ vim.opt.history       = 100
 vim.opt.lazyredraw    = true
 vim.opt.synmaxcol     = 240
 vim.opt.updatetime    = 250
+-------------------------------------------------------------
+-- Use ripgrep as grep tool:
+-------------------------------------------------------------
+vim.opt.grepprg       = "rg --vimgrep --no-heading"
+vim.opt.grepformat    = "%f:%l:%c:%m,%f:%l:%m"
 -----------------------------------------------------------
 -- Startup
 -----------------------------------------------------------
