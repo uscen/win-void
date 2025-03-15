5844aa22dcef52eebed35c39389809df2a75950a
u-shen <farah.ushen@gmail.com>
2025-03-15 04:08:00 +0000

    added tgpt

---
+++ win-dotfiles/app/local/nvim/plugin/tgpt.lua
@@ -0,0 +1,57 @@
+--          ╔═════════════════════════════════════════════════════════╗
+--          ║                       Tgpt Ai                           ║
+--          ╚═════════════════════════════════════════════════════════╝
+-- Function to create a buffer and open a window
+local createBuffer = function()
+  WIDTH = vim.api.nvim_get_option("columns")
+  HEIGHT = vim.api.nvim_get_option("lines")
+  vim.api.nvim_open_win(vim.api.nvim_create_buf(false, true), true, {
+    relative = 'editor',
+    width = math.floor(WIDTH / 5),
+    height = math.floor(HEIGHT / 1.1),
+    col = WIDTH,
+    row = 0,
+    anchor = "NE",
+    style = 'minimal',
+    border = 'single'
+  })
+end
+
+-- Function for interactive chat
+local InteractiveChat = function()
+  createBuffer()
+  vim.api.nvim_command("startinsert")
+  vim.fn.termopen("tgpt -i", {
+    on_exit = function()
+      local win_id = vim.api.nvim_get_current_win()
+      vim.api.nvim_win_close(win_id, true)
+    end
+  })
+end
+
+-- Function to rate the code in the current buffer
+local RateMyCode = function()
+  local file = vim.api.nvim_buf_get_name(0)
+  local prompt = "cat " .. file .. " | tgpt 'Rate the code' "
+  createBuffer()
+  vim.fn.termopen(prompt)
+end
+
+-- Function to check for bugs in the current buffer
+local CheckForBugs = function()
+  local file = vim.api.nvim_buf_get_name(0)
+  local prompt = "cat " .. file .. " | tgpt 'Check for bugs' "
+  createBuffer()
+  vim.fn.termopen(prompt)
+end
+
+-- Check if tgpt is installed and set up commands
+local result = vim.fn.executable("tgpt")
+if result == 1 then
+  vim.api.nvim_create_user_command("Chat", InteractiveChat, { nargs = 0 })
+  vim.api.nvim_create_user_command("RateMyCode", RateMyCode, { nargs = 0 })
+  vim.api.nvim_create_user_command("CheckForBugs", CheckForBugs, { nargs = 0 })
+else
+  print(
+    "[tgpt.nvim] tgpt is not installed on your system\nplease visit the tgpt GitHub page for instructions: https://github.com/aandrew-me/tgpt")
+end
