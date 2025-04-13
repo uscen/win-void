35c354366eae86a65f38d2fd9ef88106e9f07694
u-shen <farah.ushen@gmail.com>
2025-04-14 00:26:18 +0100

    make neovide more smooth

--- win-dotfiles/app/local/nvim/init.lua
+++ win-dotfiles/app/local/nvim/init.lua
@@ -660,7 +660,7 @@ later(function()
   if vim.g.neovide then
     -- General Animations: ==============================================================
     vim.g.neovide_refresh_rate = 60
-    vim.g.neovide_scroll_animation_length = 0.2
+    vim.g.neovide_scroll_animation_length = 0.3
     vim.g.neovide_remember_window_size = true
     vim.opt.mousescroll = "ver:10,hor:6"
     vim.opt.linespace = -1
@@ -671,13 +671,10 @@ later(function()
     vim.g.neovide_floating_z_height = 2
     vim.g.neovide_light_angle_degrees = 45
     vim.g.neovide_light_radius = 15
-    -- Blur Animations: =================================================================
-    vim.g.neovide_floating_blur_amount_x = 10.0
-    vim.g.neovide_floating_blur_amount_y = 10.0
     -- Cursor Animations: ===============================================================
     vim.o.guicursor =
     "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait100-blinkoff700-blinkon700-Cursor/lCursor,sm:block-blinkwait0-blinkoff300-blinkon300"
-    vim.g.neovide_cursor_animation_length = 0.03
+    vim.g.neovide_cursor_animation_length = 0.1
     vim.g.neovide_cursor_smooth_blink = true
     vim.g.neovide_cursor_vfx_mode = "pixiedust"
     -- Resize Fonts:  ===================================================================
