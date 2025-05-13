273071753ff54c98fa2fb1463bfa5c54c43b2db2
u-shen <farah.ushen@gmail.com>
2025-05-13 04:47:46 -0700

    simplfiy terminal

--- win-dotfiles/app/local/nvim/plugin/terminal.lua
+++ win-dotfiles/app/local/nvim/plugin/terminal.lua
@@ -1,72 +1,35 @@
 --          ╔═════════════════════════════════════════════════════════╗
 --          ║                     Toggle Terminal                     ║
 --          ╚═════════════════════════════════════════════════════════╝
-local group = vim.api.nvim_create_augroup("terminal", { clear = true })
-vim.api.nvim_create_autocmd("TermOpen", {
-  desc = "terminal options",
-  group = group,
-  callback = function()
-    vim.o.number = false
-    vim.opt_local.buflisted = false
-    vim.cmd.startinsert()
-  end,
-})
-
-local floating_terminal_state = {
-  buf = -1,
-  win = -1,
-  insert = nil,
-  winview = nil,
-}
-
-local function update_terminal_size(win)
-  local height = math.floor(vim.o.lines * 0.3)
-  vim.api.nvim_win_set_config(win, {
-    relative = "editor",
-    height = height,
-    width = vim.o.columns,
-    row = vim.o.lines - height,
-    col = 0,
-  })
-end
-
-vim.keymap.set({ "n", "t" }, "<C-t>", function()
-  if not vim.api.nvim_win_is_valid(floating_terminal_state.win) then
-    if not vim.api.nvim_buf_is_valid(floating_terminal_state.buf) then
-      floating_terminal_state.buf = vim.api.nvim_create_buf(false, true)
-    end
-    floating_terminal_state.win = vim.api.nvim_open_win(floating_terminal_state.buf, true, {
-      relative = "editor",
-      border = "single",
-      height = math.floor(vim.o.lines * 0.3),
-      width = vim.o.columns,
-      row = vim.o.lines - math.floor(vim.o.lines * 0.3),
-      col = 0,
-    })
-    vim.wo[floating_terminal_state.win].winhl = "Normal:Normal,FloatBorder:MiniPickBorder"
-
-    if vim.bo[floating_terminal_state.buf].buftype ~= "terminal" then
-      vim.cmd.terminal()
-    elseif floating_terminal_state.insert then
-      vim.cmd.startinsert()
-    else
-      vim.fn.winrestview(floating_terminal_state.winview)
-    end
-  else
-    floating_terminal_state.insert = (vim.api.nvim_get_mode().mode == "t")
-    if not floating_terminal_state.insert then
-      floating_terminal_state.winview = vim.fn.winsaveview()
-    end
-    update_terminal_size(floating_terminal_state.win)
-    vim.api.nvim_win_hide(floating_terminal_state.win)
+vim.keymap.set({ "n", "t" }, "<C-t>", (function()
+  local buf, win = nil, nil
+  local was_insert = true
+  local cfg = function()
+    return {
+      relative = 'editor',
+      width = math.floor(vim.o.columns * 0.8),
+      height = math.floor(vim.o.lines * 0.8),
+      row = math.floor(vim.o.lines * 0.1),
+      col = math.floor(vim.o.columns * 0.1),
+      style = 'minimal',
+      border = 'rounded',
+    }
   end
-end, { desc = "toggle floating terminal" })
-
-vim.api.nvim_create_autocmd("VimResized", {
-  group = group,
-  callback = function()
-    if floating_terminal_state.win ~= -1 and vim.api.nvim_win_is_valid(floating_terminal_state.win) then
-      update_terminal_size(floating_terminal_state.win)
+  return function()
+    buf = (buf and vim.api.nvim_buf_is_valid(buf)) and buf or nil
+    win = (win and vim.api.nvim_win_is_valid(win)) and win or nil
+    if not buf and not win then
+      vim.cmd("split | terminal")
+      buf = vim.api.nvim_get_current_buf()
+      vim.opt_local.buflisted = false
+      vim.api.nvim_win_close(vim.api.nvim_get_current_win(), true)
+      win = vim.api.nvim_open_win(buf, true, cfg())
+    elseif not win and buf then
+      win = vim.api.nvim_open_win(buf, true, cfg())
+    elseif win then
+      was_insert = vim.api.nvim_get_mode().mode == "t"
+      return vim.api.nvim_win_close(win, true)
     end
-  end,
-})
+    if was_insert then vim.cmd("startinsert") end
+  end
+end)(), { desc = "Toggle float terminal" })
