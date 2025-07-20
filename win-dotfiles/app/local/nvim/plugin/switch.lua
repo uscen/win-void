ba710aab4e30dc5ec9199d83bc0843c3133b08c3
u-shen <farah.ushen@gmail.com>
2025-07-20 17:56:59 +0100

    added switch plugin

---
+++ win-dotfiles/app/local/nvim/plugin/switch.lua
@@ -0,0 +1,27 @@
+--          ╔═════════════════════════════════════════════════════════╗
+--          ║                     Toggle Word                         ║
+--          ╚═════════════════════════════════════════════════════════╝
+local function toggle()
+  local toggles = {
+    ["true"] = "false",
+    ["yes"] = "no",
+    ["up"] = "down",
+    ["left"] = "right",
+    ["top"] = "bottom",
+    ["always"] = "never",
+    ["disable"] = "enable",
+  }
+
+  local cword = vim.fn.expand("<cword>")
+  local newWord
+  for word, opposite in pairs(toggles) do
+    if cword == word then newWord = opposite end
+    if cword == opposite then newWord = word end
+  end
+  if newWord then
+    local prevCursor = vim.api.nvim_win_get_cursor(0)
+    vim.cmd.normal { '"_ciw' .. newWord, bang = true }
+    vim.api.nvim_win_set_cursor(0, prevCursor)
+  end
+end
+vim.keymap.set('n', '<leader>r', toggle, { noremap = true, silent = true, desc = "Toggle word under cursor" })
