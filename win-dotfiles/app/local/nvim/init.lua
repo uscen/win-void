f83c2d4597b1069ceba122d83fe0c5613f274265
u-shen <farah.ushen@gmail.com>
2025-07-20 17:02:45 +0100

    notifactions in bottom

--- win-dotfiles/app/local/nvim/init.lua
+++ win-dotfiles/app/local/nvim/init.lua
@@ -75,7 +75,12 @@ end)
 --          │                     Mini.Notify                         │
 --          ╰─────────────────────────────────────────────────────────╯
 later(function()
-  require('mini.notify').setup()
+  local win_config = function()
+    local has_statusline = vim.o.laststatus > 0
+    local pad = vim.o.cmdheight + (has_statusline and 1 or 0)
+    return { anchor = 'SE', col = vim.o.columns, row = vim.o.lines - pad }
+  end
+  require('mini.notify').setup({ window = { config = win_config } })
   vim.notify = require('mini.notify').make_notify()
 end)
 --          ╭─────────────────────────────────────────────────────────╮
