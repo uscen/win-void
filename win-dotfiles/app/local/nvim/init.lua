2814c2e9d2a1acb6a9d283990af4736de0246d51
u-shen <farah.ushen@gmail.com>
2025-07-27 15:11:33 +0100

    remove statusline

--- win-dotfiles/app/local/nvim/init.lua
+++ win-dotfiles/app/local/nvim/init.lua
@@ -314,43 +314,6 @@ now(function()
   })
 end)
 --          ╭─────────────────────────────────────────────────────────╮
---          │                     Mini.Statusline                     │
---          ╰─────────────────────────────────────────────────────────╯
-now(function()
-    local mini_statusline = require("mini.statusline")
-    mini_statusline.setup({
-        content = {
-            active = function()
-                local mode, mode_hl = mini_statusline.section_mode({ trunc_width = 100 })
-                local filename = mini_statusline.section_filename({ trunc_width = 100 })
-                local fileinfo = mini_statusline.section_fileinfo({ trunc_width = 999 })
-                local location = mini_statusline.is_truncated(100) and '%2l│%-2v' or '%2l/%-2L│%2v/%-2{virtcol("$") - 1}'
-
-                return mini_statusline.combine_groups({
-                    {
-                        hl = mode_hl,
-                        strings = { mode },
-                    },
-                    "%<",
-                    {
-                        hl = "MiniStatuslineFilename",
-                        strings = { filename },
-                    },
-                    "%=",
-                    {
-                        hl = "MiniStatuslineFileinfo",
-                        strings = { fileinfo },
-                    },
-                    {
-                        hl = mode_hl,
-                        strings = { location },
-                    },
-                })
-            end,
-        },
-    })
-end)
---          ╭─────────────────────────────────────────────────────────╮
 --          │                     Mini.Starter                        │
 --          ╰─────────────────────────────────────────────────────────╯
 now(function()
@@ -806,7 +769,7 @@ now(function()
   vim.opt.showmode               = false
   vim.opt.ruler                  = false
   vim.opt.numberwidth            = 3
-  vim.opt.laststatus             = 3
+  vim.opt.laststatus             = 0
   vim.opt.cmdheight              = 0
   vim.opt.winwidth               = 20
   vim.opt.winminwidth            = 5
