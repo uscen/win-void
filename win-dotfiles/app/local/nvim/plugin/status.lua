795058060e056097850880ec2cbd8dd692698735
u-shen <farah.ushen@gmail.com>
2025-08-03 23:02:00 +0100

    fix comment

--- win-dotfiles/app/local/nvim/plugin/status.lua
+++ win-dotfiles/app/local/nvim/plugin/status.lua
@@ -36,13 +36,13 @@ local function pad_string(str, left_pad, right_pad)
   local right = string.rep(" ", right_pad or 0)
   return left .. str .. right
 end
--- a function to obtain file type:============================================================================
+-- a function to obtain file type: =================================================================================
 local function filetype()
   local ft = vim.bo.filetype
   local ft_icons_or_name = filetype_icons[ft] and filetype_icons[ft] or ""
   return "%#StatuslineFade#".. fade_start .. "%#statusline_filetype#" .. pad_string(ft_icons_or_name, 1,3) .. "%*"
 end
--- a function to obtain and format the file name:==============================================================
+-- a function to obtain and format the file name: ==================================================================
 local function file_name()
   local filename = string.format(vim.fn.expand("%:t")):upper()
   if filename == "" then
@@ -57,12 +57,12 @@ local function file_name()
   if vim.bo.buftype == "TERMINAL" then
     filename = "TERMINAL"
   end
-  -- change highlight group based on if the file has been modified:=============================================
+  -- change highlight group based on if the file has been modified: =============================================
   local highlight_group = vim.bo.modified and filename ~= "[no name]" and "statusline_modifiedfile" or "statusline_file"
   local modified_indicator = vim.bo.modified and '' or " "
   return "%#" .. highlight_group .. "# " .. pad_string(filename, 2,1).. modified_indicator .. " " .. "%#StatuslineFade1#".. fade_start
 end
--- a function to obtain and format the current mode::========================================================
+-- a function to obtain and format the current mode: ===========================================================
 local function current_mode()
   local mode = vim.fn.mode()
   local mode_aliases = {
@@ -78,7 +78,7 @@ local function current_mode()
   mode = mode and mode_aliases[mode] and mode_aliases[mode]:upper() or "?"
   return "%#statusline_mode#" .. pad_string(mode, 2,1) .. " " .. "%#StatuslineFade#".. fade_end
 end
--- a function to obtain and format the LSP:==================================================================
+-- a function to obtain and format the LSP: ====================================================================
 local function stbufnr()
   return vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
 end
@@ -92,12 +92,12 @@ local function lsp()
   end
   return "%#StatuslineFade1#".. fade_end .. "%#statusline_misc# " .. pad_string("󰄭  NOLSP", 1, 2)
 end
--- a function to assign highlight group to the separator====================================================
+-- a function to assign highlight group to the separator: ======================================================
 local function separator()
   local highlight_group = "statusline_separator"
   return "%#" .. highlight_group .. "#%="
 end
--- a function to call and place the statusline components:==================================================
+-- a function to call and place the statusline components: =====================================================
 function Status_line()
   local curr_ft = vim.bo.filetype
   local disabled_filetypes = {
@@ -114,7 +114,7 @@ function Status_line()
     filetype()
   })
 end
--- default with statusline but can be toggled with <leader>st:===============================================
+-- default with statusline but can be toggled with <leader>st: ================================================
 vim.keymap.set( "n", "<leader>st",
   function()
     if vim.o.laststatus == 0 then
@@ -125,7 +125,7 @@ vim.keymap.set( "n", "<leader>st",
   end
 )
 vim.cmd("set statusline=%!v:lua.Status_line()")
--- set colors for each statusline components:==============================================================
+-- set colors for each statusline components: =================================================================
 local group_styles = {
   ["Statusline"]                  = { fg = "#181616", bg = "#181616" },
   ["statusline_diagnostics"]      = { fg = "#c5c9c5", bg = "#141b1e" },
