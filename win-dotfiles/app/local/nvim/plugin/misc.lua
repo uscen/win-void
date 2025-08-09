c65e6ca6205d8640fdb47157877ef0856c1b69c8
u-shen <farah.ushen@gmail.com>
2025-08-09 16:16:33 +0100

    add more misc

--- win-dotfiles/app/local/nvim/plugin/misc.lua
+++ win-dotfiles/app/local/nvim/plugin/misc.lua
@@ -2,7 +2,26 @@
 --          ║                         Misc                            ║
 --          ╚═════════════════════════════════════════════════════════╝
 local M = {}
+-- Open url in buffer: ==============================================================================================
+function M.openUrlInBuffer()
+  local text = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), '\n')
+  local urls = {}
+  for url in text:gmatch([[%l%l%l+://[^%s)%]}"'`>]+]]) do
+    urls[#urls + 1] = url
+  end
+  if #urls == 0 then
+    return vim.notify('No URL found in file.', vim.log.levels.WARN)
+  elseif #urls == 1 then
+    return vim.ui.open(urls[1])
+  end
+  vim.ui.select(urls, { prompt = ' Open URL:' }, function(url)
+    if url then vim.ui.open(url) end
+  end)
+end
 
+vim.api.nvim_create_user_command('OpenUrlInBuffer', M.openUrlInBuffer, {})
+
+-- Smart duplicate line: ==============================================================================================
 function M.smartDuplicate()
   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
   local line = vim.api.nvim_get_current_line()
@@ -34,3 +53,28 @@ function M.smartDuplicate()
 end
 
 vim.api.nvim_create_user_command('SmartDuplicate', M.smartDuplicate, {})
+
+-- Smart duplicate line: ==============================================================================================
+function M.lspCapabilities()
+  local clients = vim.lsp.get_clients { bufnr = 0 }
+  if #clients == 0 then
+    vim.notify('No LSPs attached.', vim.log.levels.WARN, { icon = '󱈄' })
+    return
+  end
+  vim.ui.select(clients, {
+    prompt = '󱈄 Select LSP:',
+    format_item = function(client) return client.name end,
+  }, function(client)
+    if not client then return end
+    local info = {
+      capabilities = client.capabilities,
+      server_capabilities = client.server_capabilities,
+      config = client.config,
+    }
+    local opts = { icon = '󱈄', title = client.name .. ' capabilities', ft = 'lua' }
+    local header = '-- For a full view, open in notification history.\n'
+    vim.notify(header .. vim.inspect(info), vim.log.levels.DEBUG, opts)
+  end)
+end
+
+vim.api.nvim_create_user_command('LSPCapabilities', M.lspCapabilities, {})
