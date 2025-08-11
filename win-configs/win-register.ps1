6f0696ef82a3733c660ebfa3c172d66f2bb68139
u-shen <farah.ushen@gmail.com>
2025-08-11 23:14:52 +0100

    change keyrate

--- win-configs/win-register.ps1
+++ win-configs/win-register.ps1
@@ -3,7 +3,7 @@
 # =============================================================================== #
 Set-Location "HKCU:\Control Panel\Accessibility\Keyboard Response"
 Set-ItemProperty -Path . -Name AutoRepeatDelay       -Value 130
-Set-ItemProperty -Path . -Name AutoRepeatRate        -Value 3
+Set-ItemProperty -Path . -Name AutoRepeatRate        -Value 2
 Set-ItemProperty -Path . -Name DelayBeforeAcceptance -Value 0
 Set-ItemProperty -Path . -Name BounceTime            -Value 0
-Set-ItemProperty -Path . -Name Flags                 -Value 59
+Set-ItemProperty -Path . -Name Flags                 -Value 47
