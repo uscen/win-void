f07e262ca4ea769628faea7b091acd63712b90b5
u-shen <farah.ushen@gmail.com>
2025-08-17 04:11:12 -0700

    add center in insert

--- win-dotfiles/app/local/nvim/init.lua
+++ win-dotfiles/app/local/nvim/init.lua
@@ -1324,6 +1324,15 @@ now_if_args(function()
       end
     end,
   })
+  -- Center when inserting: ======================================================================
+  vim.api.nvim_create_autocmd('InsertEnter', {
+      callback = function()
+          local debounce = 8
+          if vim.fn.abs(vim.fn.line('.') - math.floor(vim.fn.line('w0') + vim.fn.winheight(0) / 2)) >= debounce then
+              vim.cmd('norm! zz')
+          end
+      end,
+  })
   -- Close all non-existing buffers on `FocusGained`: ============================================
   vim.api.nvim_create_autocmd('FocusGained', {
     group = vim.api.nvim_create_augroup('close_non_existing_buffer', { clear = true }),
@@ -1594,6 +1603,18 @@ later(function()
     end
     vim.notify('Format On Save Disable')
   end, { bang = true, })
+  -- format: =====================================================================================
+  vim.api.nvim_create_user_command('Format', function(args)
+      local range = nil
+      if args.count ~= -1 then
+          local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+          range = {
+              start = { args.line1, 0 },
+              ['end'] = { args.line2, end_line:len() },
+          }
+      end
+      require('conform').format({ async = true, lsp_format = 'fallback', range = range })
+  end, { range = true })
 end)
 --              ╭─────────────────────────────────────────────────────────╮
 --              │                Neovim misspelled_commands               │
