45bb6271cf87b3a65c7cba0110424f501b80d724
u-shen <farah.ushen@gmail.com>
2025-08-22 05:38:34 -0700

    edit completion background

--- win-dotfiles/app/roming/qutebrowser/config/config.py
+++ win-dotfiles/app/roming/qutebrowser/config/config.py
@@ -5,6 +5,7 @@ config.load_autoconfig(False)
 #               ╔═════════════════════════════════════════════════════════╗
 #               ║                          Fonts                          ║
 #               ╚═════════════════════════════════════════════════════════╝
+# UI ==================================================================================
 c.fonts.default_size = '10pt'
 c.fonts.default_family = "JetBrainsMono Nerd Font"
 c.fonts.completion.entry = '10pt "JetBrainsMono Nerd Font"'
@@ -13,30 +14,30 @@ c.fonts.prompts = 'default_size sans-serif'
 c.fonts.statusbar = '10pt "JetBrainsMono Nerd Font"'
 c.fonts.tabs.selected = "10pt JetBrainsMono Nerd Font"
 c.fonts.tabs.unselected = "10pt JetBrainsMono Nerd Font"
+# Web =================================================================================
+c.fonts.web.size.default = 20
 c.fonts.web.family.fixed = 'monospace'
 c.fonts.web.family.sans_serif = 'monospace'
 c.fonts.web.family.serif = 'monospace'
 c.fonts.web.family.standard = 'monospace'
-c.fonts.web.size.default = 20
 #               ╔═════════════════════════════════════════════════════════╗
 #               ║                         Statusbar                       ║
 #               ╚═════════════════════════════════════════════════════════╝
 c.statusbar.show = 'in-mode'
 c.statusbar.position = 'bottom'
 c.statusbar.widgets = ['progress', 'keypress', 'url', 'scroll', 'history', 'tabs']
-
 #               ╔═════════════════════════════════════════════════════════╗
 #               ║                             Tabs                        ║
 #               ╚═════════════════════════════════════════════════════════╝
 c.tabs.position = "left"
-c.tabs.width = 36
+c.tabs.width = 26
 c.tabs.show = "multiple"
 c.tabs.max_width = 240
 c.tabs.min_width = 100
 c.tabs.favicons.show = "always"
 c.tabs.favicons.scale=1
-c.tabs.padding = {"top": 2, "bottom": 2, "left": 2, "right": 2}
-c.tabs.indicator.width = 2
+c.tabs.padding = {"top": 4, "bottom": 4, "left": 4, "right": 4}
+c.tabs.indicator.width = 0
 c.tabs.wrap = True
 c.tabs.background = True
 c.tabs.last_close = "default-page"
@@ -70,14 +71,27 @@ c.content.webrtc_ip_handling_policy = "default-public-interface-only"
 #               ║                          URL                            ║
 #               ╚═════════════════════════════════════════════════════════╝
 c.url.searchengines = {
-   'DEFAULT': 'https://duckduckgo.com/?q={}',
-   '!aw': 'https://wiki.archlinux.org/?search={}',
-   '!gh': 'https://github.com/search?o=desc&q={}&s=stars',
-   '!yt': 'https://www.youtube.com/results?search_query={}',
+    "DEFAULT": "https://duckduckgo.com/?q={}",
+    "p":  "https://engine.presearch.org/search?q={}",
+    "b":  "https://search.brave.com/search?q={}",
+    "d":  "https://duckduckgo.com/?q={}",
+    "s":  "https://www.startpage.com/do/asearch?q={}",
+    "g":  "https://google.com/search?q={}",
+    "pr": "https://www.protondb.com/search?q={}",
+    "aw": "https://wiki.archlinux.org/?search={}",
+    "al": "https://allegro.pl/listing?string={}",
+    "yt": "https://www.youtube.com/results?search_query={}",
+    "th": "https://www.thingiverse.com/search?q={}",
+    "at": "https://alternativeto.net/browse/search?q={}",
+    "th":  "https://duckduckgo.com/?q={} site:thingiverse.com",
+    "tr":  "https://translate.google.pl/?text={}",
 }
 #               ╔═════════════════════════════════════════════════════════╗
 #               ║                       Keybidings                        ║
 #               ╚═════════════════════════════════════════════════════════╝
+config.bind('<Ctrl-0>', 'zoom')
+config.bind('<Ctrl-=>', 'zoom-in')
+config.bind('<Ctrl-->', 'zoom-out')
 config.bind('zi', 'zoom-in')
 config.bind('zo', 'zoom-out')
 config.bind('gJ', 'tab-move +')
@@ -90,6 +104,7 @@ config.bind('T', 'hint links tab')
 config.bind('si', 'hint images download')
 config.bind('tT', 'config-cycle tabs.position top left')
 config.bind('cs', 'cmd-set-text -s :config-source')
+config.bind('ce', 'config-edit')
 config.bind('<ctrl-y>', 'spawn --userscript ytdl.sh')
 config.bind('<Ctrl-j>', 'completion-item-focus --history next', mode='command')
 config.bind('<Ctrl-k>', 'completion-item-focus --history prev', mode='command')
@@ -101,6 +116,8 @@ c.colors.webpage.darkmode.enabled = True
 c.colors.webpage.preferred_color_scheme = "dark"
 c.colors.webpage.darkmode.policy.images = "smart"
 c.colors.webpage.darkmode.policy.page = "smart"
+c.colors.webpage.darkmode.threshold.background = 100
+c.colors.webpage.darkmode.threshold.foreground = 150
 # Palette ==============================================================================
 bg0_hard   = "#0d0c0c"
 bg0_normal = "#181616"
@@ -120,97 +137,97 @@ cyan       = "#658594"
 comment    = "#727169"
 
 # Completion ===========================================================================
-c.colors.completion.fg                     = fg0
-c.colors.completion.odd.bg                 = bg0_hard
-c.colors.completion.even.bg                = bg0_hard
-c.colors.completion.category.fg            = blue
-c.colors.completion.category.bg            = bg1
-c.colors.completion.category.border.top    = bg1
-c.colors.completion.category.border.bottom = bg1
-c.colors.completion.item.selected.fg       = fg0
-c.colors.completion.item.selected.bg       = bg3
-c.colors.completion.item.selected.border.top    = bg2
-c.colors.completion.item.selected.border.bottom = bg2
-c.colors.completion.match.fg               = orange
-c.colors.completion.scrollbar.fg           = fg0
-c.colors.completion.scrollbar.bg           = bg1
+c.colors.completion.fg                            = fg0
+c.colors.completion.odd.bg                        = bg0_normal
+c.colors.completion.even.bg                       = bg0_normal
+c.colors.completion.category.fg                   = green
+c.colors.completion.category.bg                   = bg0_soft
+c.colors.completion.category.border.top           = bg1
+c.colors.completion.category.border.bottom        = bg1
+c.colors.completion.item.selected.fg              = fg0
+c.colors.completion.item.selected.bg              = bg1
+c.colors.completion.item.selected.border.top      = bg2
+c.colors.completion.item.selected.border.bottom   = bg2
+c.colors.completion.match.fg                      = green
+c.colors.completion.scrollbar.fg                  = fg0
+c.colors.completion.scrollbar.bg                  = bg1
 # Context menu ========================================================================
-c.colors.contextmenu.menu.bg      = bg0_hard
-c.colors.contextmenu.menu.fg      = fg1
-c.colors.contextmenu.disabled.bg  = bg2
-c.colors.contextmenu.disabled.fg  = fg1
-c.colors.contextmenu.selected.bg  = bg3
-c.colors.contextmenu.selected.fg  = fg1
+c.colors.contextmenu.menu.bg                      = bg0_normal
+c.colors.contextmenu.menu.fg                      = fg1
+c.colors.contextmenu.disabled.bg                  = bg0_hard
+c.colors.contextmenu.disabled.fg                  = fg1
+c.colors.contextmenu.selected.bg                  = bg1
+c.colors.contextmenu.selected.fg                  = fg1
 # Downloads ===========================================================================
-c.colors.downloads.bar.bg           = bg0_hard
-c.colors.downloads.start.fg         = bg0_hard
-c.colors.downloads.start.bg         = green
-c.colors.downloads.stop.fg          = fg0
-c.colors.downloads.stop.bg          = cyan
-c.colors.downloads.error.fg         = red
+c.colors.downloads.bar.bg                         = bg0_hard
+c.colors.downloads.start.fg                       = bg0_hard
+c.colors.downloads.start.bg                       = green
+c.colors.downloads.stop.fg                        = fg0
+c.colors.downloads.stop.bg                        = cyan
+c.colors.downloads.error.fg                       = red
 # Hints & Keyhint =====================================================================
-c.colors.hints.fg            = bg0_hard
-c.colors.hints.bg            = f"rgba({int(orange[1:3],16)}, {int(orange[3:5],16)}, {int(orange[5:7],16)}, 200)"
-c.colors.hints.match.fg      = bg3
-c.colors.keyhint.fg          = fg1
-c.colors.keyhint.suffix.fg   = fg0
-c.colors.keyhint.bg          = bg0_hard
+c.colors.hints.fg                                 = bg0_hard
+c.colors.hints.bg                                 = f"rgba({int(orange[1:3],16)}, {int(orange[3:5],16)}, {int(orange[5:7],16)}, 200)"
+c.colors.hints.match.fg                           = bg3
+c.colors.keyhint.fg                               = fg1
+c.colors.keyhint.suffix.fg                        = fg0
+c.colors.keyhint.bg                               = bg0_hard
 # Messages ============================================================================
-c.colors.messages.error.fg    = bg0_hard
-c.colors.messages.error.bg    = red
-c.colors.messages.error.border= red
-c.colors.messages.warning.fg  = bg0_hard
-c.colors.messages.warning.bg  = purple
-c.colors.messages.warning.border = purple
-c.colors.messages.info.fg     = fg1
-c.colors.messages.info.bg     = bg0_hard
-c.colors.messages.info.border = bg0_hard
+c.colors.messages.error.fg                        = bg0_hard
+c.colors.messages.error.bg                        = red
+c.colors.messages.error.border                    = red
+c.colors.messages.warning.fg                      = bg0_hard
+c.colors.messages.warning.bg                      = purple
+c.colors.messages.warning.border                  = purple
+c.colors.messages.info.fg                         = fg1
+c.colors.messages.info.bg                         = bg0_hard
+c.colors.messages.info.border                     = bg0_hard
 # Prompts =============================================================================
-c.colors.prompts.fg           = fg1
-c.colors.prompts.bg           = bg2
-c.colors.prompts.border       = f"1px solid {bg1}"
-c.colors.prompts.selected.bg  = bg3
+c.colors.prompts.fg                               = fg1
+c.colors.prompts.bg                               = bg2
+c.colors.prompts.border                           = f"1px solid {bg1}"
+c.colors.prompts.selected.bg                      = bg3
 # Statusbar ===========================================================================
-c.colors.statusbar.normal.fg       = fg1
-c.colors.statusbar.normal.bg       = bg0_normal
-c.colors.statusbar.insert.fg       = bg0_hard
-c.colors.statusbar.insert.bg       = green
-c.colors.statusbar.passthrough.fg  = bg0_hard
-c.colors.statusbar.passthrough.bg  = blue
-c.colors.statusbar.private.fg      = purple
-c.colors.statusbar.private.bg      = bg0_hard
-c.colors.statusbar.command.fg      = fg1
-c.colors.statusbar.command.bg      = bg1
-c.colors.statusbar.command.private.fg = purple
-c.colors.statusbar.command.private.bg = bg1
-c.colors.statusbar.caret.fg        = bg0_hard
-c.colors.statusbar.caret.bg        = purple
-c.colors.statusbar.caret.selection.fg = bg0_hard
-c.colors.statusbar.caret.selection.bg = purple
-c.colors.statusbar.url.fg          = fg1
-c.colors.statusbar.url.error.fg    = red
-c.colors.statusbar.url.warn.fg     = orange
-c.colors.statusbar.url.success.http.fg  = red
-c.colors.statusbar.url.success.https.fg = fg0
-c.colors.statusbar.url.hover.fg    = orange
-c.colors.statusbar.progress.bg     = green
+c.colors.statusbar.normal.fg                      = fg1
+c.colors.statusbar.normal.bg                      = bg0_normal
+c.colors.statusbar.insert.fg                      = bg0_hard
+c.colors.statusbar.insert.bg                      = green
+c.colors.statusbar.passthrough.fg                 = bg0_hard
+c.colors.statusbar.passthrough.bg                 = blue
+c.colors.statusbar.private.fg                     = purple
+c.colors.statusbar.private.bg                     = bg0_hard
+c.colors.statusbar.command.fg                     = fg1
+c.colors.statusbar.command.bg                     = bg1
+c.colors.statusbar.command.private.fg             = purple
+c.colors.statusbar.command.private.bg             = bg1
+c.colors.statusbar.caret.fg                       = bg0_hard
+c.colors.statusbar.caret.bg                       = purple
+c.colors.statusbar.caret.selection.fg             = bg0_hard
+c.colors.statusbar.caret.selection.bg             = purple
+c.colors.statusbar.url.fg                         = fg1
+c.colors.statusbar.url.error.fg                   = red
+c.colors.statusbar.url.warn.fg                    = orange
+c.colors.statusbar.url.success.http.fg            = red
+c.colors.statusbar.url.success.https.fg           = fg0
+c.colors.statusbar.url.hover.fg                   = orange
+c.colors.statusbar.progress.bg                    = green
 # Tabs ================================================================================
-c.colors.tabs.bar.bg              = bg0_normal
-c.colors.tabs.odd.fg              = fg1
-c.colors.tabs.odd.bg              = bg2
-c.colors.tabs.even.fg             = fg1
-c.colors.tabs.even.bg             = bg3
-c.colors.tabs.selected.odd.fg     = fg1
-c.colors.tabs.selected.odd.bg     = bg0_hard
-c.colors.tabs.selected.even.fg    = fg1
-c.colors.tabs.selected.even.bg    = bg0_hard
-c.colors.tabs.pinned.even.bg      = green
-c.colors.tabs.pinned.even.fg      = bg2
-c.colors.tabs.pinned.odd.bg       = green
-c.colors.tabs.pinned.odd.fg       = bg2
-c.colors.tabs.pinned.selected.even.bg = bg0_hard
-c.colors.tabs.pinned.selected.even.fg = fg1
-c.colors.tabs.pinned.selected.odd.bg  = bg0_hard
-c.colors.tabs.pinned.selected.odd.fg  = fg1
+c.colors.tabs.bar.bg                              = bg0_normal
+c.colors.tabs.odd.fg                              = fg1
+c.colors.tabs.odd.bg                              = bg0_normal
+c.colors.tabs.even.fg                             = fg1
+c.colors.tabs.even.bg                             = bg0_normal
+c.colors.tabs.selected.odd.fg                     = fg1
+c.colors.tabs.selected.odd.bg                     = bg0_hard
+c.colors.tabs.selected.even.fg                    = fg1
+c.colors.tabs.selected.even.bg                    = bg0_hard
+c.colors.tabs.pinned.even.bg                      = green
+c.colors.tabs.pinned.even.fg                      = bg2
+c.colors.tabs.pinned.odd.bg                       = green
+c.colors.tabs.pinned.odd.fg                       = bg2
+c.colors.tabs.pinned.selected.even.bg             = bg0_hard
+c.colors.tabs.pinned.selected.even.fg             = fg1
+c.colors.tabs.pinned.selected.odd.bg              = bg0_hard
+c.colors.tabs.pinned.selected.odd.fg              = fg1
 # Webpage ==============================================================================
 c.colors.webpage.bg = bg0_soft
