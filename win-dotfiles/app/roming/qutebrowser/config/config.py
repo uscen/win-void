c06a92cb780ea768572fe99fc112f9e921b3021d
u-shen <farah.ushen@gmail.com>
2025-08-31 04:48:51 -0700

    add keys

--- win-dotfiles/app/roming/qutebrowser/config/config.py
+++ win-dotfiles/app/roming/qutebrowser/config/config.py
@@ -212,14 +212,15 @@ c.content.persistent_storage = False
 c.content.plugins = False
 c.content.dns_prefetch = False
 c.content.register_protocol_handler = False
+c.content.proxy = 'system'
 c.content.webrtc_ip_handling_policy = "default-public-interface-only"
 c.content.default_encoding = "utf-8"
 c.content.tls.certificate_errors = 'ask-block-thirdparty'
 c.content.unknown_url_scheme_policy = 'allow-from-user-interaction'
 # headers =============================================================================
-c.content.headers.do_not_track = True
-c.content.headers.user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
-c.content.headers.accept_language = 'en-US,en;q=0.9'
+c.content.headers.do_not_track = False
+c.content.headers.user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
+c.content.headers.accept_language = 'en-US,en,ca-ES,de'
 c.content.headers.referer = 'same-domain'
 # Headers per site ====================================================================
 config.set("content.headers.user_agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) QtWebEngine/6.4.2 Chrome/102.0.5005.177 Safari/537.36", "twitter.com")
@@ -273,9 +274,16 @@ c.session.default_name = "default"
 c.new_instance_open_target = "tab-bg-silent"
 c.new_instance_open_target_window = "last-focused"
 #               ╔═════════════════════════════════════════════════════════╗
+#               ║                          FileSelect                     ║
+#               ╚═════════════════════════════════════════════════════════╝
+# c.fileselect.handler = 'external'
+# c.fileselect.folder.command         = ["alacritty", "-e", "yazi", "--choosedir={}"]
+# c.fileselect.multiple_files.command = ["alacritty", "-e", "yazi", "--choosefiles={}"]
+# c.fileselect.single_file.command    = ["alacritty", "-e", "yazi", "--choosefile={}"]
+#               ╔═════════════════════════════════════════════════════════╗
 #               ║                          Others                         ║
 #               ╚═════════════════════════════════════════════════════════╝
-c.scrolling.smooth=False
+c.scrolling.smooth=True
 c.scrolling.bar = 'never'
 c.confirm_quit=["downloads"]
 c.editor.command = ["alacritty", "-e", "nvim", "{file}"]
@@ -448,9 +456,11 @@ config.bind('<Ctrl-Shift-i>', 'devtools right')
 config.bind('T', 'hint links tab')
 config.bind('dd', 'tab-close')
 config.bind('do', 'tab-only')
+config.bind('dh', 'tab-only --next --pinned keep')
+config.bind('dl', 'tab-only --prev --pinned keep')
 config.bind('dp', 'tab-pin')
 config.bind('dm', 'tab-mute')
-config.bind('dl', 'tab-focus last')
+config.bind('dn', 'tab-focus last')
 config.bind('dc', 'tab-only ;; home')
 config.bind('dJ', 'tab-move +')
 config.bind('dK', 'tab-move -')
@@ -482,6 +492,16 @@ config.bind(',Y', 'hint links spawn alacritty -e yt-dlp {hint-url}')
 config.bind('yM', 'yank ;; spawn mpv {url}')
 config.bind('<Ctrl-Shift-m>', 'spawn mpv {url}')
 config.bind('<Ctrl-m>', 'hint links spawn mpv {hint-url}')
+# Binds for images ====================================================================
+config.unbind(';i')
+config.bind(';ii', 'hint images')
+config.bind(';io', 'hint images run open {hint-url}')
+config.bind(';iO', 'hint images run open -t {hint-url}')
+config.bind(';iy', 'hint images yank')
+config.bind(';iY', 'hint images yank-primary')
+config.bind(';ig', 'hint images run open https://www.google.com/searchbyimage?&image_url={hint-url}')
+# Bind for opening link in private window ============================================
+config.bind(';p', 'hint all run open -p {hint-url}')
 # Keybindings: Stylesheets ============================================================
 config.bind(',c', 'config-cycle content.user_stylesheets "" ""')
 config.bind(',r', 'config-cycle content.user_stylesheets "~/.config/qutebrowser/styles/nord-all-sites.css" "~/.config/qutebrowser/styles/solarized-dark-all-sites.css" "~/.config/qutebrowser/styles/solarized-light-all-sites.css"  "" ')
