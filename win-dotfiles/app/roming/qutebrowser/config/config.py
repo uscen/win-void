5a6522a8775d9f5404b64c9897a0a32e90183b60
u-shen <farah.ushen@gmail.com>
2025-09-03 11:20:32 +0100

    fixit

--- win-dotfiles/app/roming/qutebrowser/config/config.py
+++ win-dotfiles/app/roming/qutebrowser/config/config.py
@@ -117,14 +117,14 @@ c.input.media_keys = True
 c.input.insert_mode.auto_enter = True
 c.input.insert_mode.auto_leave = True
 c.input.insert_mode.leave_on_load = True
-c.input.insert_mode.auto_load = False
+c.input.insert_mode.auto_load = True
 c.input.insert_mode.plugins = False
 c.input.links_included_in_focus_chain = True
 c.input.mouse.rocker_gestures = False
 c.input.spatial_navigation = False
 c.input.partial_timeout = 30000
 c.input.forward_unbound_keys = 'all'
-c.input.mode_override = "normal"
+c.input.mode_override = None
 #               ╔═════════════════════════════════════════════════════════╗
 #               ║                         Search                          ║
 #               ╚═════════════════════════════════════════════════════════╝
