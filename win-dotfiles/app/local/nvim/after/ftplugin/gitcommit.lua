7ed1d46884bd934b6038a0edb1211628013edd9c
u-shen <farah.ushen@gmail.com>
2025-09-08 01:16:50 +0100

    add full name

--- win-dotfiles/app/local/nvim/after/ftplugin/gitcommit.lua
+++ win-dotfiles/app/local/nvim/after/ftplugin/gitcommit.lua
@@ -1,3 +1,3 @@
-vim.cmd('setlocal nonumber nornu')
+vim.cmd('setlocal nonumber norelativenumber')
 vim.cmd('setlocal spell wrap')
 vim.cmd('setlocal foldmethod=expr foldexpr=v:lua.MiniGit.diff_foldexpr() foldlevel=1')
