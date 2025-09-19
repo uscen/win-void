3f0a2dc8f98c8e03e1d5f308375fa6dcb1e75617
uxhen <farah.ushen@gmail.com>
2025-09-20 00:30:42 +0100

    add neovide cursor color

--- win-dotfiles/app/local/nvim/init.lua
+++ win-dotfiles/app/local/nvim/init.lua
@@ -1814,6 +1814,7 @@ later(function()
     vim.g.neovide_hide_mouse_when_typing = true
     vim.g.neovide_cursor_animate_in_insert_mode = false
     vim.g.neovide_cursor_animate_command_line = false
+    vim.cmd("highlight! Normal guifg=#87a987")
     -- cursor: ===================================================================================
     vim.g.neovide_position_animation_length = 0
     vim.g.neovide_cursor_animation_length = 0.00
