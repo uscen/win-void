3eb0ae0e9fadbe415eda85923b8a06fee0fbfb21
uxhen <farah.ushen@gmail.com>
2025-09-25 16:58:44 +0100

    add spelllang

--- win-dotfiles/app/local/nvim/after/ftplugin/gitcommit.lua
+++ win-dotfiles/app/local/nvim/after/ftplugin/gitcommit.lua
@@ -5,6 +5,7 @@
 vim.opt_local.number = false
 vim.opt_local.relativenumber = false
 vim.opt_local.spell = true
+vim.opt_local.spelllang = "en_us"
 vim.opt_local.wrap = true
 vim.opt_local.foldmethod = 'expr'
 vim.opt_local.foldexpr = 'v:lua.MiniGit.diff_foldexpr()'
