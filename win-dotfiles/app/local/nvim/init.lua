6bd157a3cec2f519f51dabba491edb748e7f3f5e
uxhen <farah.ushen@gmail.com>
2025-09-27 14:30:28 +0100

    add awesome nvim

--- win-dotfiles/app/local/nvim/init.lua
+++ win-dotfiles/app/local/nvim/init.lua
@@ -863,16 +863,7 @@ now(function()
   vim.wo.signcolumn              = 'yes'
   vim.opt.statuscolumn           = ''
   vim.opt.fillchars              = 'eob: ,fold: ,foldopen:,foldsep: ,foldclose:'
-  vim.opt.listchars              = {
-    eol = '↲',
-    tab = '→ ',
-    trail = '+',
-    extends = '›',
-    precedes = '‹',
-    space = '•',
-    nbsp =
-    '␣',
-  }
+  vim.opt.listchars              = { eol = '↲', tab = '→ ', trail = '+', extends = '›', precedes = '‹', space = '•', nbsp = '␣', }
   -- Editing:  ===================================================================================
   vim.opt.cindent                = true
   vim.opt.autoindent             = true
