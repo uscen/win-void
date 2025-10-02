d4647a7c515bf1168dec68415a0315f03565a5f8
uxhen <farah.ushen@gmail.com>
2025-10-02 13:10:04 +0100

    add trimspaces commands

--- win-dotfiles/app/local/nvim/init.lua
+++ win-dotfiles/app/local/nvim/init.lua
@@ -1553,6 +1553,17 @@ later(function()
     vim.notify(path)
     vim.fn.setreg('+', path)
   end, {})
+  -- TrimSpaces and LastLine: ====================================================================
+  vim.api.nvim_create_user_command("TrimSpaces", function()
+      local curpos = vim.api.nvim_win_get_cursor(0)
+      vim.cmd([[keeppatterns %s/\s\+$//e]])
+      vim.api.nvim_win_set_cursor(0, curpos)
+  end, {})
+  vim.api.nvim_create_user_command("TrimLastLines", function()
+      local n_lines = vim.api.nvim_buf_line_count(0)
+      local last_nonblank = vim.fn.prevnonblank(n_lines)
+      if last_nonblank < n_lines then vim.api.nvim_buf_set_lines(0, last_nonblank, n_lines, true, {}) end
+  end, {})
   -- Toggle conform.nvim auto-formatting: ========================================================
   vim.api.nvim_create_user_command('ToggleFormat', function()
     vim.g.autoformat = not vim.g.autoformat
