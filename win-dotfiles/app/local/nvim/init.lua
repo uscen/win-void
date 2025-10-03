0cf28c6e288adc0d857bfb37abadf957016423db
uxhen <farah.ushen@gmail.com>
2025-10-03 23:46:42 +0100

    disable autoformat

--- win-dotfiles/app/local/nvim/init.lua
+++ win-dotfiles/app/local/nvim/init.lua
@@ -839,18 +839,7 @@ now(function()
   vim.o.shortmess                = 'FOSWIaco'
   vim.wo.signcolumn              = 'yes'
   vim.o.statuscolumn             = ''
-  vim.o.fillchars                = table.concat(
-  {
-    'eob: ',
-    'fold:╌',
-    'horiz:═',
-    'horizdown:╦',
-    'horizup:╩',
-    'vert:║',
-    'verthoriz:╬',
-    'vertleft:╣',
-    'vertright:╠'
-  }, ',')
+  vim.o.fillchars                = table.concat( { 'eob: ', 'fold:╌', 'horiz:═', 'horizdown:╦', 'horizup:╩', 'vert:║', 'verthoriz:╬', 'vertleft:╣', 'vertright:╠' }, ',')
   vim.o.listchars                = table.concat({ 'extends:…', 'nbsp:␣', 'precedes:…', 'tab:> ' }, ',')
   -- Editing:  ===================================================================================
   vim.o.cindent                  = true
@@ -907,45 +896,9 @@ now(function()
   vim.o.virtualedit              = 'block'
   vim.o.formatoptions            = 'rqnl1j'
   vim.o.formatexpr               = "v:lua.require'conform'.formatexpr()"
-  vim.o.sessionoptions           = table.concat(
-  {
-    'blank',
-    'buffers',
-    'curdir',
-    'folds',
-    'help',
-    'tabpages',
-    'winsize',
-    'terminal',
-    'localoptions'
-  }, ',')
-  vim.o.diffopt                  = table.concat(
-  {
-    'algorithm:minimal',
-    'closeoff',
-    'context:8',
-    'filler',
-    'internal',
-    'linematch:100',
-    'indent-heuristic'
-  }, ',')
-  vim.o.suffixesadd              = table.concat(
-  {
-    '.css',
-    '.html',
-    '.js',
-    '.json',
-    '.jsx',
-    '.lua',
-    '.md',
-    '.rs',
-    '.scss',
-    '.sh',
-    '.ts',
-    '.tsx',
-    '.yaml',
-    '.yml'
-  }, ',')
+  vim.o.sessionoptions           = table.concat( { 'blank', 'buffers', 'curdir', 'folds', 'help', 'tabpages', 'winsize', 'terminal', 'localoptions' }, ',')
+  vim.o.diffopt                  = table.concat( { 'algorithm:minimal', 'closeoff', 'context:8', 'filler', 'internal', 'linematch:100', 'indent-heuristic' }, ',')
+  vim.o.suffixesadd              = table.concat( { '.css', '.html', '.js', '.json', '.jsx', '.lua', '.md', '.rs', '.scss', '.sh', '.ts', '.tsx', '.yaml', '.yml' }, ',')
   -- Folds:  =====================================================================================
   vim.o.foldenable               = false
   vim.o.foldlevel                = 1
@@ -1860,8 +1813,7 @@ later(function()
   vim.keymap.set('n', '[c', function() require('mini.diff').goto_hunk('prev') end)
   vim.keymap.set('n', ']c', function() require('mini.diff').goto_hunk('next') end)
   -- Explorer: ====================================================================================
-  vim.keymap.set('n', '<leader>e',
-    function() require('mini.files').open(vim.bo.buftype ~= 'nofile' and vim.api.nvim_buf_get_name(0) or nil, true) end)
+  vim.keymap.set('n', '<leader>e', function() require('mini.files').open(vim.bo.buftype ~= 'nofile' and vim.api.nvim_buf_get_name(0) or nil, true) end)
   vim.keymap.set('n', '<leader>E', function() require('mini.files').open(vim.uv.cwd(), true) end)
 end)
 --              ╔═════════════════════════════════════════════════════════╗
